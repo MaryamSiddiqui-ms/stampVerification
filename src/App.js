8f4f8a5ba8b909e3bd27172882c99799690078d1
MaryamSiddiqui-ms <ms.maryamsiddiqui@.com>
2023-05-13 14:25:58 +0100

    image-module

--- src/App.js
+++ src/App.js
@@ -1,64 +1,102 @@
-    import React, { Component } from 'react';
-    import './App.css';
-    import leftImage from './assets/left.jpg';
-    import Typewriter from "typewriter-effect";
+import React, { useState } from 'react';
+import './App.css';
+import leftImage from './assets/left.jpg';
+import Typewriter from "typewriter-effect";
+import Modal from 'react-modal';
 
-    function Footer() {
-    return (
-      <footer>
-        <p className='Footer'>&copy; 2023 Deep Learning with Perception</p>
-      </footer>
-    );
-    }
-    function App() {
-    return (
-      <div className="App">
-        <div className="split left">
-          
-          <div className="split top">
+function ImageViewModal(props) {
+  return (
+    <Modal isOpen={props.isOpen} onRequestClose={props.onClose} 
+    overlayClassName="overlay"
+    className="modal">
+      <button onClick={props.onClose} className="close-button">X</button>
+      {props.file && <img src={URL.createObjectURL(props.file)} alt="Uploaded" />}
+    </Modal>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p className='Footer'>&copy; 2023 Deep Learning with Perception</p>
+    </footer>
+  );
+}
+
+function App() {
+  const [uploadedFile, setUploadedFile] = useState(null);
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  
+  const closeModal = () => {
+    setUploadedFile(null);
+    setIsModalOpen(false);
+  };
+  const hiddenFileInput = React.useRef(null);
+
+  const handleClick = event => {
+    hiddenFileInput.current.click();
+  };
+
+  const handleChange = (event) => {
+    const fileUploaded = event.target.files[0];
+    setUploadedFile(fileUploaded);
+    setIsModalOpen(true);
+  };
+
+  return (
+    <div className="App">
+      <div className="split left">
+        <div className="split top">
           <div className="image-container">
             <img src={leftImage} alt="left" />
+          </div>
+        </div>
+        <div className="split bottom">
+          <div class="students-container">
+            <div class="student">
+              <p class="roll-number">K200397</p>
+              <p class="name">Mohammad Bilal Aziz</p>
+            </div>
+            <div class="student">
+              <p class="roll-number">K200434</p>
+              <p class="name">Maryam Siddiqui</p>
+            </div>
+            <div class="student">
+              <p class="roll-number">K201680</p>
+              <p class="name">Owais Ali</p>
             </div>
           </div>
-          <div className="split bottom">
-              <div class="students-container">
-    <div class="student">
-      <p class="roll-number">K200397</p>
-      <p class="name">Mohammad Bilal Aziz</p>
-    </div>
-    <div class="student">
-      <p class="roll-number">K200434</p>
-      <p class="name">Maryam Siddiqui</p>
-    </div>
-    <div class="student">
-      <p class="roll-number">K201680</p>
-      <p class="name">Owais Ali</p>
-    </div>
-    </div>
-
-            </div>
           <Footer /> {/* Add Footer component */}
         </div>
-        <div className="split right">
-          <h1 className="main-heading">STAMP VERIFICATION</h1>
-          <div className= "sub-heading">
-          <Typewriter 
-                  onInit={(typewriter)=> {
-                    typewriter
-                      .typeString("Image Segmenation\n")
-                      .pauseFor(1000)
-                      .start();
-                  }}
-                /> 
-    </div>
-    <div>
-            <p className="sub-sub-heading">Upload your image to verify its stamp</p>
-            </div>
-            <button className="upload-button">Upload Image</button>
-        </div>
-
       </div>
-    );
-    }
+      <div className="split right">
+        <h1 className="main-heading">STAMP VERIFICATION</h1>
+        <div className="sub-heading">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Image Segmentation\n")
+                .pauseFor(1000)
+                .start();
+            }}
+          />
+        </div>
+        <div>
+          <p className="sub-sub-heading">Upload your image to verify its stamp</p>
+        </div>
+        <button onClick={handleClick} className="upload-button">Upload Image</button>
+        <input
+          type="file"
+          ref={hiddenFileInput}
+          onChange={handleChange}
+          style={{ display: 'none' }}
+          accept="image/*"
+        />
+        <ImageViewModal isOpen={isModalOpen} onClose={closeModal} file={uploadedFile} />
+      </div>
+    </div>
+  );
+}
 
-    export default App;
+export default App;
